a517bde63605acd99474b0122ef5a6e73ef267a2
laiminghai <laiminghai@mzjmedia.net>
2018-12-17 09:04:45 +0800

    modify gateway.

--- api-gateway/src/main/java/gdou/laiminghai/apigateway/filter/TokenFilter.java
+++ api-gateway/src/main/java/gdou/laiminghai/apigateway/filter/TokenFilter.java
@@ -4,9 +4,14 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class TokenFilter implements GlobalFilter, Ordered {
@@ -20,8 +25,13 @@ public class TokenFilter implements GlobalFilter, Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String token = exchange.getRequest().getQueryParams().getFirst("token");
         if (token == null || token.isEmpty()) {
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
+            ServerHttpResponse originalResponse = exchange.getResponse();
+            originalResponse.setStatusCode(HttpStatus.OK);
+            originalResponse.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+            byte[] response = "{\"code\":\"401\",\"msg\":\"401 Unauthorized.\"}"
+                    .getBytes(StandardCharsets.UTF_8);
+            DataBuffer buffer = originalResponse.bufferFactory().wrap(response);
+            return originalResponse.writeWith(Flux.just(buffer));
         }
         return chain.filter(exchange);
     }
